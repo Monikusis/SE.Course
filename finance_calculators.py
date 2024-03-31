f177c80641a8c78a30dd66c70040c293752d38ad
Monikusis <155104899+Monikusis@users.noreply.github.com>
2024-03-31 19:38:58 +0100

    Update finance_calculators.py

--- finance_calculators.py
+++ finance_calculators.py
@@ -11,61 +11,44 @@ Use formula
 Print out answer
 """
 
-# Welcome message
-print("Investment - to calculate the amount of interest you'll earn on your investment")
-print("Bond - to calculate the amount you'll have to pay on a home loan")
+# Welcome message and user input prompt for selecting either 'investment' or 'bond'.
+print("Investment - Calculate interest earned on investment.")
+print("Bond - Calculate monthly bond repayment.")
 print(" ")
 user_choice = input("Enter either 'investment' or 'bond' from the menu above to proceed: ").lower()
 
 # Investment option
 if user_choice == 'investment':
-    
-
+    # Prompt for deposit amount, interest rate, and investment duration.
     deposit_amount = int(input("Please enter the amount you are depositing: "))
-    interest_rate = int(input("Please enter your provided interest rate in percentages: "))
-    investment_years = int(input("How many years you will be investing? "))  
+    interest_rate = int(input("Please enter the provided interest rate in percentages: "))
+    investment_years = int(input("How many years will you be investing? "))
     
-    # 'Simple' or ' Compound' option
+    # Prompt for selecting between 'simple' or 'compound' interest.
     simple_compound = input("Would you like 'simple' or 'compound' interest? ")
 
-    interest = simple_compound
-    
-    simple_interest =  deposit_amount*(1 + (interest_rate/100)*investment_years)
-    compound_interest = deposit_amount * math.pow((1 + interest_rate/100),investment_years)
-    
-    if simple_interest == "simple":
+    # Calculate simple or compound interest based on user choice.
+    if simple_compound == "simple":
+        simple_interest = deposit_amount * (1 + (interest_rate / 100) * investment_years)
         print(f"Your return is {simple_interest}")
     else:
+        import math
+        compound_interest = deposit_amount * math.pow((1 + interest_rate / 100), investment_years)
         print(f"Your return is {compound_interest}")
 
-#'Bond' option
+# Bond option
 elif user_choice == 'bond':
     print("Thank you!")
-
-    house_value = int(input("Please present value of the house: "))
-    monthly_rate = int(input("Your monthly interest rate: "))
-    repayment_months = int(input("The number of months to repay the bond: "))   
     
-    repayment = (((monthly_rate/12) / 100) * house_value) / (1 - (1 + (monthly_rate/12) / 100) ** (-repayment_months))
+    # Prompt for house value, monthly interest rate, and repayment duration.
+    house_value = int(input("Please enter the present value of the house: "))
+    monthly_rate = int(input("Enter your monthly interest rate: "))
+    repayment_months = int(input("Enter the number of months to repay the bond: "))   
+    
+    # Calculate monthly bond repayment.
+    repayment = (((monthly_rate / 12) / 100) * house_value) / (1 - (1 + (monthly_rate / 12) / 100) ** (-repayment_months))
     print(f"Your expected monthly repayment is {repayment}.")
-    
-# Error message 
+
+# Error message for invalid input
 else:
-    print("Sorry you entered invalid data, please choose from above.")
-
- 
-    
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
+    print("Sorry, you entered invalid data. Please choose from the options above.")
